1eb123e14aae1a5adcb2df3d039df458002b762d
Tharunraj Optimus <59702671+tharunoptimus@users.noreply.github.com>
2021-10-23 16:36:48 +0530

    Added global styles

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -20,6 +20,22 @@ const Home: NextPage = () => {
         <p className={styles.title}>This is the landing page</p>
 
       </main>
+
+      <style global jsx>{`
+      
+      html,
+      body {
+        padding: 0;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        min-height: 100%;
+      }
+      
+      #__next {
+        min-height: 100%;
+      }
+      `}</style>
     </div>
 
   )
