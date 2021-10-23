c1712eb1e8576b7901a2ebffa6664034fa9050a6
Tharunraj Optimus <59702671+tharunoptimus@users.noreply.github.com>
2021-10-23 16:39:13 +0530

    Added styles to detect page

--- front-end/pages/detect.tsx
+++ front-end/pages/detect.tsx
@@ -52,7 +52,91 @@ const Home: NextPage = () => {
             </div>
 
 
-        </div>     
+        </div>
+
+
+        <style jsx>{`
+        
+            .wrapper {
+                display: flex;
+                flex-direction: column;
+                min-height: 99vh;
+                padding: 1vh;
+            }
+
+            .heading {
+                min-height: 5rem;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+
+            .title {
+                text-align: center;
+                padding: 0.5rem 2rem;
+                border: 1px solid #44444444;
+            }
+
+            .title span {
+                font-size: 2.5rem;
+                text-align: center;
+            }
+
+            .videoFeed {
+                flex: 1;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+
+            .videoFeed video {
+                width: 550px;
+            }
+
+            .dataAndOptions {
+                display: flex;
+                justify-content: space-evenly;
+                align-items: center;
+                flex-wrap: wrap;
+            }
+
+            .scorebox {
+                padding: 0.5rem;
+                border: 1px solid #44444444;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                min-width: 260px;
+            }
+
+            .scoreTitle {
+                font-size: 2rem;
+                font-weight: 500;
+            }
+
+            .score {
+                font-size: 4rem;
+                color: #F03737;
+            }
+
+            .captureButtonContainer button {
+                font-size: 1.5rem;
+                background-color: #FFFFFF;
+                border: 1px solid #44444444;
+                padding: 0.5rem 1rem;
+                border-radius: 5px;
+                cursor: pointer;
+            }
+
+            .captureButtonContainer button:hover {
+                transform: scale(1.1);
+                transition: all 0.1s ease-in-out;
+            }
+
+        
+        `}</style>
+      
       
       
       </>
